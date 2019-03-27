d16eb4a85891d4c4bd408d18aa1d948af7b90422
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2019-03-27 08:14:30 +0100

    more flexible db access, bump sebipom to 1.4.3

--- daodemo/dbscripts/loaddb.sh
+++ daodemo/dbscripts/loaddb.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 db=fluidbusiness
 scriptdir=$(dirname $0)
+server=localhost
 props=${scriptdir}/../connection.properties
 if [ -e ${props} ]; then
     source ${scriptdir}/../connection.properties
 fi
-dropdb --if-exists ${db}
-createdb -O exam ${db}
-cat ${scriptdir}/schema.sql | psql -X ${db} 
-cat ${scriptdir}/newpiet.sql | psql -X ${db} 
+dropdb -h ${server}  -U postgres --if-exists ${db}  
+createdb -h ${server} -U postgres -O ${dbuser} ${db}
+
+cat ${scriptdir}/schema.sql | psql -h ${server} -U postgres -X ${db} 
+cat ${scriptdir}/newpiet.sql | psql -h ${server} -U postgres  -X ${db} 
