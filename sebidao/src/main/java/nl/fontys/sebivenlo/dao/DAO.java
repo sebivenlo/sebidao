d31bdcf1fd510dccf73694f3ad183551c6288399
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2019-04-14 10:15:40 +0200

    added/improved doc

--- sebidao/src/main/java/nl/fontys/sebivenlo/dao/DAO.java
+++ sebidao/src/main/java/nl/fontys/sebivenlo/dao/DAO.java
@@ -149,12 +149,15 @@ public interface DAO<K extends Serializable, E extends Entity2<K>> extends AutoC
      * versions of the entities, generated fields and all.
      *
      * The caller is advised to give this DAO a transaction token, so this
-     * operation can perform atomically by rolling back any progress for those
-     * persistence implementations that support commit and rollback.
+     * operation can perform atomically for those persistence implementations
+     * that support proper commit and rollback.
      *
+     * Implementations that recreate resources on every call of
+     * {@code dao.save(e)} should probably overwrite this default implementation
+     * for improved performance and memory efficiency.
      *
      * @param entities to save
-     * @return the saved enities
+     * @return the saved entities
      * @since 0.4
      */
     default Collection<E> saveAll( Iterable<E> entities ) {
@@ -164,9 +167,12 @@ public interface DAO<K extends Serializable, E extends Entity2<K>> extends AutoC
 
     /**
      * Save the given entities, returning the result as a list.
-     *
+     * 
+     * This default implementation will benefit from any improvemenst by an overwritten
+     * {@code saveAll(Collection &lt;E&gt;entities)}
      * @param entities to save
      * @return the saved versions of the entities in a list.
+     * @since 0.4
      */
     default Collection<E> saveAll( E... entities ) {
 
