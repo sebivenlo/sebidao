1b0fb53e6598257b33cf5e0234c09defa54077c3
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2019-11-06 11:58:08 +0100

    added example

--- sebidao/src/test/java/memclient/MemClientTest.java
+++ sebidao/src/test/java/memclient/MemClientTest.java
@@ -1,36 +1,40 @@
-
 package memclient;
 
 import entities.Employee;
 import entities.EmployeeMapper2;
+import nl.fontys.sebivenlo.dao.memory.InMemoryDAO;
 import nl.fontys.sebivenlo.dao.memory.MemoryDAOFactory;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Note that entities saved in the dao are automagically persisted to disk on (test) JVM shutdown.
+ * 
  * @author hom
  */
 public class MemClientTest {
-    nl.fontys.sebivenlo.dao.DAO<Integer,Employee> dao;
+
+    nl.fontys.sebivenlo.dao.DAO<Integer, Employee> dao;
+    MemoryDAOFactory fac = MemoryDAOFactory.getInstance();
+
     public MemClientTest() {
-        MemoryDAOFactory fac = MemoryDAOFactory.getInstance();
         fac.registerMapper( Employee.class, new EmployeeMapper2() );
-        dao = fac.createDao( Employee.class);
-        
+        dao = fac.createDao( Employee.class );
+
     }
- 
+
 //    @Ignore
     @Test
     public void testIBi() {
-        Employee ibi = new Employee(42,"Kouzak","Ibrahim","i@there.com",4);
-        
+        Employee ibi = new Employee( 42, "Kouzak", "Ibrahim", "i@there.com", 4 );
+
         Employee saveDIbi = dao.save( ibi );
-        
-        assertNotNull(saveDIbi);
-        
-        assertFalse(saveDIbi.toString().isEmpty());
+
+        assertNotNull( saveDIbi );
+
+        assertFalse( saveDIbi.toString().isEmpty() );
         System.out.println( "saveDIbi = " + saveDIbi );
-        fail( "testIBi reached it's and. You will know what to do." );
+//        fail( "testIBi reached it's and. You will know what to do." );
     }
+    
 }
