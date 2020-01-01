604b46588237b766334aec3dc58c5c4e14133a14
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2020-01-01 11:31:59 +0100

    bumped deps versions

--- sebidao/src/test/java/memclient/MemClientTest.java
+++ sebidao/src/test/java/memclient/MemClientTest.java
@@ -2,30 +2,31 @@ package memclient;
 
 import entities.Employee;
 import entities.EmployeeMapper2;
+import nl.fontys.sebivenlo.dao.DAO;
 import nl.fontys.sebivenlo.dao.memory.InMemoryDAO;
 import nl.fontys.sebivenlo.dao.memory.MemoryDAOFactory;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Note that entities saved in the dao are automagically persisted to disk on (test) JVM shutdown.
- * 
+ * Note that entities saved in the dao are automagically persisted to disk on
+ * (test) JVM shutdown.
+ *
  * @author hom
  */
 public class MemClientTest {
 
-    nl.fontys.sebivenlo.dao.DAO<Integer, Employee> dao;
     MemoryDAOFactory fac = MemoryDAOFactory.getInstance();
 
     public MemClientTest() {
         fac.registerMapper( Employee.class, new EmployeeMapper2() );
-        dao = fac.createDao( Employee.class );
 
     }
 
 //    @Ignore
     @Test
     public void testIBi() {
+        DAO<Integer, Employee> dao = fac.createDao( Employee.class );
         Employee ibi = new Employee( 42, "Kouzak", "Ibrahim", "i@there.com", 4 );
 
         Employee saveDIbi = dao.save( ibi );
@@ -36,5 +37,5 @@ public class MemClientTest {
         System.out.println( "saveDIbi = " + saveDIbi );
 //        fail( "testIBi reached it's and. You will know what to do." );
     }
-    
+
 }
