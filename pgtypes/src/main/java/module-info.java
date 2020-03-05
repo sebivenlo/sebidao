65ef45e29adaabc1e8877887fe78da505b7335c9
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2020-03-05 10:57:45 +0100

    modular, pgtaypes package renamed to pgranges

---
+++ pgtypes/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+
+open module nl.fontys.sebivenlo.pgranges {
+    requires org.postgresql.jdbc;
+    requires transitive java.sql;
+    requires java.base;
+    //opens nl.fontys.sebivenlo.pgranges;
+}
